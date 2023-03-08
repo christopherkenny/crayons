b7462bd75fd5ac32fc3784940c23f61293623adf
Christopher Kenny <ctk44@cornell.edu>
2023-03-08 11:45:59 -0500

    plot generic

--- R/utils.R
+++ R/utils.R
@@ -18,16 +18,18 @@ bocks <- function(r, c) {
   seqc <- seq_len(c) - 1L
 
   list(
-    x = c(vapply(seqr, \(row) rep(row + c(0, 1, 1, 0), times = r), FUN.VALUE = numeric(4 * r))),
-    y = c(t(vapply(seqc, \(col) rep(col + c(0, 0, 1, 1), times = c), FUN.VALUE = numeric(4 * c))))
+    x = rep(vapply(seqc, \(row) row + c(0, 1, 1, 0), FUN.VALUE = numeric(4)), r),
+    y = c(vapply(seqr, \(col) rep(col + c(0, 0, 1, 1), times = c), FUN.VALUE = numeric(4 * c)))
   )
   # needs to be more like 0, 0, 1, 1,     1, 1, 2, 2,     2, 2, 3, 3,     3, 3, 4, 4, ...
 }
 
+#' @export
 plot.palette <- function(x, ...) {
-  # this is a budget version of `scales::show_col()` in ggplot
-  # how many colors
+  # this is my budget version of `scales::show_col()` in ggplot
+
   n <- length(x)
+  x_in <- setNames(x, x)
 
   # convert to square-able
   nc <- ceiling(sqrt(n))
@@ -42,7 +44,25 @@ plot.palette <- function(x, ...) {
     y = box$y
   )
 
+  label_loc <- data.frame(
+    col = x,
+    x = box$x[seq(1, length(x) * 4, by = 4)] + 0.5,
+    y = box$y[seq(1, length(x) * 4, by = 4)] + 0.5
+  )
+  label_loc$col[is.na(label_loc$col)] <- ''
+
+  if (requireNamespace("farver", quietly = TRUE)) {
+    label_loc$color = ifelse(farver::decode_colour(x, to = 'hcl')[, 3] > 50, 'black', 'white')
+  } else {
+    label_loc$color = 'black'
+  }
+
   ggplot2::ggplot(sq, ggplot2::aes(x = x, y = y)) +
     ggplot2::geom_polygon(ggplot2::aes(fill = col)) +
-    ggplot2::geom_text(ggplot2::aes(label = col))
+    ggplot2::geom_text(data = label_loc, ggplot2::aes(label = col, color = color)) +
+    ggplot2::guides(fill = 'none', color = 'none') +
+    ggplot2::scale_fill_manual(values = x_in, na.value = 'white') +
+    ggplot2::scale_color_manual(values = c('black' = 'black', 'white' = 'white')) +
+    ggplot2::coord_fixed() +
+    ggplot2::theme_void()
 }
